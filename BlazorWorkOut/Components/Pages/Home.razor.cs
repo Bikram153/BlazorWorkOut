430ce20e14c839a89e4b02a331f56bfad2f9418f
Bikram99999 <bikram.samanta@nevaehtech.com>
2025-07-03 18:29:05 +0530

    rendering implementation

---
+++ BlazorWorkOut/Components/Pages/Home.razor.cs
@@ -0,0 +1,33 @@
+﻿using BlazorWorkOut.Models;
+namespace BlazorWorkOut.Components.Pages
+{
+    public partial class Home
+    {
+        private List<User>? user;
+        private Dictionary<string, object> attributes = new Dictionary<string, object>
+        {
+            {"placeholder","First Name" },
+            {"disabled","disabled" }
+        };
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(5000);
+            //user = new List<User>();
+            user = new List<User>
+            {
+                new User
+                {
+                    UserName = "Bikram",
+                    Email = "bikram@gmail.com"
+                },
+                new User
+                {
+                    UserName = "Sayan",
+                    Email = "sayan@gmail.com"
+                }
+            };
+            await base.OnInitializedAsync();
+        }
+
+    }
+}
